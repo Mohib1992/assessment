1fe94151128d772a48c07e80fb791c9ffa77aa04
Md.Mohib Ullah <moin.uoda@gmail.com>
2014-12-23 21:10:24 +0600

    moin:modify database settings

---
+++ config/development/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moinkhan
+ * Date: 12/23/2014
+ * Time: 9:02 PM
+ */
+
+return[
+
+    'mysql' => array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'db_pillers',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    )
+];
